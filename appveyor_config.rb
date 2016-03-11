603144bd8690ae8bdc2070118818f055b91819da
bggd <gameovera@live.com>
2016-03-11 19:27:04 +0900

    Add appveyor.yml, appveyor_config.rb

---
+++ appveyor_config.rb
@@ -0,0 +1,14 @@
+MRuby::Build.new do |conf|
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+  end
+
+  enable_debug
+
+  conf.gembox 'full-core'
+
+  conf.enable_bintest
+  conf.enable_test
+end
